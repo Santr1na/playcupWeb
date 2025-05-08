945558e1498b555ec80ff7255df7a44cee17d9f7
Santrina <f.klokov00@gmail.com>
2025-05-08 20:22:07 +0300

    nine commit

--- src/component/pages/Tournaments/Tournaments.js
+++ src/component/pages/Tournaments/Tournaments.js
@@ -3,10 +3,18 @@ import './Tournaments.css';
 import Header from "../../Header/Header";
 import Footer from "../../Footer/Footer";
 import ActiveGame from '../../Menu/ActiveGame/ActiveGame';
+import CardTournament from "../../cardsTournaments/cardTournaments";
 
 
 function Tournaments(){
     const [value, setValue] = useState('');
+
+    const [activeTour, setActiveTour] = useState('future'); // начальное значение 'future'
+
+    // Функция для управления состоянием активного тура
+    const handleTourClick = (tourType) => {
+      setActiveTour(tourType);
+    };
     return(
         <>
             <Header />
@@ -22,20 +30,59 @@ function Tournaments(){
             </svg>
 
             <main>
-            <div class="center">
-                <div class="up-tournaments-page">
+            <div className="center">
+                <div className="up-tournaments-page">
                     <ActiveGame />
 
-                    <button class="create-tournaments" translate="no">
+                    <button className="create-tournaments" translate="no">
                         <p>Создать турнир</p>
                     </button>
                         
-                    <div class="back-search-tournaments">
-                        <input class="search-tournaments" type="text" placeholder="Поиск" value={value} onChange={(e) => setValue(e.target.value)}/>
+                    <div className="back-search-tournaments">
+                        <input className="search-tournaments" type="text" placeholder="Поиск" value={value} onChange={(e) => setValue(e.target.value)}/>
                     </div>
                 </div>
 
-</div>
+                <div className="main-tournaments-page">
+                <CardTournament />
+                <div className="filters-tournaments-page">
+                    <div className="up-filters-tournaments-page">
+                        <div className={`future-tour ${activeTour === 'future' ? 'active' : ''}`}
+                            onClick={() => handleTourClick('future')}>
+                            Будущие
+                        </div>
+                        <div className="line-filters-tournaments-page"></div>
+                        <div className={`now-tour ${activeTour === 'now' ? 'active' : ''}`}
+                            onClick={() => handleTourClick('now')}>
+                            Нынешние
+                        </div>
+                        <div className="line-filters-tournaments-page"></div>
+                        <div className={`past-tour ${activeTour === 'past' ? 'active' : ''}`}
+                            onClick={() => handleTourClick('past')}>
+                            Прошедшие
+                        </div>
+                    </div>
+                    <div className="down-filters-tournaments-page">
+                        <span className="text-filter-tour">Фильтры</span>
+                        <div className="filter-region">
+                            <span>Регионы</span>
+                            <svg id="pol" className="dropdown-arrow-filter-region" width="9" height="7" viewBox="0 0 9 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M7.08826 0.330127C7.85806 0.330127 8.33919 1.16346 7.95429 1.83013L5.35621 6.33013C4.97131 6.99679 4.00906 6.99679 3.62416 6.33013L1.02608 1.83013C0.641184 1.16346 1.12231 0.330127 1.89211 0.330127L7.08826 0.330127Z" fill="white"/>
+                            </svg>
+                        </div>
+                        <div className="filter-format">
+                            <span>Формат</span>
+                            <svg id="pol" className="dropdown-arrow-filter-format" width="9" height="7" viewBox="0 0 9 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M7.08826 0.330127C7.85806 0.330127 8.33919 1.16346 7.95429 1.83013L5.35621 6.33013C4.97131 6.99679 4.00906 6.99679 3.62416 6.33013L1.02608 1.83013C0.641184 1.16346 1.12231 0.330127 1.89211 0.330127L7.08826 0.330127Z" fill="white"/>
+                            </svg>
+                        </div>
+                        <div className="button-reset-tour">
+                            <button className="reset-filter-tour">Сброс</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
             </main>
 
             <Footer />
